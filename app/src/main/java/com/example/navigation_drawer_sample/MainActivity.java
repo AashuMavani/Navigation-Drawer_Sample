c39e8aeda84e0765a384ff6f7df58a397d8436c0
asmita <asmitamavani804@gmail.com>
2023-06-29 21:43:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigation_drawer_sample/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.navigation_drawer_sample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigationview);
+
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,drawerLayout,toolbar,R.string.Open_Drawer,R.string.Close_Drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                if (id == R.id.item_Home) {
+                    addFragment(new Home_Fragment());
+                }
+                 if (id == R.id.item_Setting) {
+                    addFragment(new Setting_Fragment());
+                }
+                if(id==R.id.item_Logout) {
+                    addFragment(new Logout_Fragment());
+                }
+            drawerLayout.close();
+                return true;
+            }
+        });
+    }
+
+    private void addFragment(Fragment fragment)
+    {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.contentview,fragment);
+        transaction.commit();
+    }
+}
